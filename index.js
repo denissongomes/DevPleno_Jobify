e9b74ddf9dbe346bde4d1348204d3905ba853680
Denisson Gomes <dnsgom@gmail.com>
2020-10-27 08:55:07 -0300

    Add post router

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-)
+
 
 const sqlite3 = require('sqlite3').verbose();
 const dbConnection = new sqlite3.Database('./database.sqlite', (err) => {
